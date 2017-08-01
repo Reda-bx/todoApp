8c925291278cbff1b0abcea41a6d5030887c344d
Reda Belahrach <reeda.bx@gmail.com>
2017-08-01 09:47:35 +0100

    remove useless title

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container">
     <div class="header">
-      <h1>ok</h1>
       <span class="total">Done {{remaining}} / {{todos.length}}</span>
       <ul class="filters">
         <li @click="visibility = 'allTodos'" :class="{ selected: visibility === 'allTodos' }">All</li>
